ffed1db979d3724ea51dfab54c1ff1e5a4714e5b
Ryan Punwasi <ryan.punwasi@gmail.com>
2024-07-10 17:23:39 -0400

    Use Cloudfront to fetch files instead of pre-signed URLs due to compulsory expiration

--- src/app/components/Download.tsx
+++ src/app/components/Download.tsx
@@ -1,16 +1,11 @@
 import { FaHeadphonesAlt } from "react-icons/fa";
 import { cookies } from "next/headers";
 import DownloadLink from "./DownloadLink";
-import { s3 } from "../lib/s3Client";
-import { GetObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const Download = async () => {
   const keyCookie = cookies().get("key");
   const Key = keyCookie?.value as string;
-  const command = new GetObjectCommand({ Key, Bucket: process.env.AWS_BUCKET });
-  const url = await getSignedUrl(s3, command, { expiresIn: 300 });
-
+  const url = `${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL}${Key}`;
   return (
     <div className="flex flex-col gap-10">
       <div className="bg-gray-900 text-gray-400 border-4 border-gray-800 gap-5 border-dashed rounded-lg flex flex-col justify-center items-center rounded-l w-96 h-48">
--- src/app/components/DownloadLink.tsx
+++ src/app/components/DownloadLink.tsx
@@ -1,12 +1,20 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const DownloadLink = async ({ signedURL }: { signedURL: string }) => {
-  const [blob, setBlob] = useState<Blob | null>(
-    new Blob([signedURL], {
-      type: "audio/mp3;charset=utf-8;",
-    })
-  );
+const DownloadLink = ({ signedURL }: { signedURL: string }) => {
+  const [blob, setBlob] = useState<Blob | null>(null);
+
+  useEffect(() => {
+    fetch(signedURL)
+      .then(response => {
+        response.blob().then(blob => {
+          setBlob(blob);
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, [signedURL]);
 
   const downloadBlob = (name = `audio.mp3`) => {
     // Convert your blob into a Blob URL (a special url that points to an object in the browser's memory)
@@ -33,12 +41,24 @@ const DownloadLink = async ({ signedURL }: { signedURL: string }) => {
   };
 
   return (
-    <button
-      onClick={() => downloadBlob()}
-      className="flex items-center justify-center gap-2 w-fit bg-violet-700 rounded-lg mx-auto py-2 px-5 filter hover:brightness-125 transition duration-200 ease-in-out text-white text-sm font-light tracking-wider shadow-md cursor-pointer active:brightness-95 disabled:cursor-not-allowed disabled:select-none disabled:hover:brightness-75 disabled:brightness-75"
-    >
-      Download
-    </button>
+    <>
+      {blob && (
+        <button
+          onClick={() => downloadBlob()}
+          className="flex items-center justify-center gap-2 w-fit bg-violet-700 rounded-lg mx-auto py-2 px-5 filter hover:brightness-125 transition duration-200 ease-in-out text-white text-sm font-light tracking-wider shadow-md cursor-pointer active:brightness-95 disabled:cursor-not-allowed disabled:select-none disabled:hover:brightness-75 disabled:brightness-75"
+        >
+          Download
+        </button>
+      )}
+      {!blob && (
+        <button
+          disabled={true}
+          className="flex items-center justify-center gap-2 w-fit bg-violet-700 rounded-lg mx-auto py-2 px-5 filter hover:brightness-125 transition duration-200 ease-in-out text-white text-sm font-light tracking-wider shadow-md cursor-pointer active:brightness-95 disabled:cursor-not-allowed disabled:select-none disabled:hover:brightness-75 disabled:brightness-75"
+        >
+          Fetching file...
+        </button>
+      )}
+    </>
   );
 };
 export default DownloadLink;
