0b25b057daf9bca921b200af5b9be69de641cd33
Ryan Punwasi <ryan.punwasi@gmail.com>
2024-07-12 02:50:43 -0400

    Move rendering logic into try/catch block

--- src/app/cut/page.tsx
+++ src/app/cut/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { GetObjectAttributesCommand } from "@aws-sdk/client-s3";
 import { s3 } from "../lib/s3Client";
 import WaveFormVisualizerContainer from "../components/WaveFormVisualizer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 const InvalidFileKeyScreen = () => {
@@ -34,23 +35,23 @@ export default async function CutPage() {
     const command = new GetObjectAttributesCommand({
       Bucket,
       Key,
-      ObjectAttributes: ["ETag"],
+      ObjectAttributes: ["ObjectSize"],
     });
     await s3.send(command);
+
+    return (
+      <main
+        className={`flex min-w-full min-h-full flex-col items-center justify-center gap-y-20 tracking-wide font-semibold ${inter.className}`}
+      >
+        <div className="h-fit md:w-1/2 w-4/5 flex justify-center items-center">
+          <WaveFormVisualizerContainer
+            s3Key={keyCookie.value}
+            src={`https://d2w8muq8s9pt97.cloudfront.net/${keyCookie.value}`}
+          />
+        </div>
+      </main>
+    );
   } catch (error: any) {
     return <InvalidFileKeyScreen />;
   }
-
-  return (
-    <main
-      className={`flex min-w-full min-h-full flex-col items-center justify-center gap-y-20 tracking-wide font-semibold ${inter.className}`}
-    >
-      <div className="h-fit md:w-1/2 w-4/5 flex justify-center items-center">
-        <WaveFormVisualizerContainer
-          s3Key={keyCookie.value}
-          src={`https://d2w8muq8s9pt97.cloudfront.net/${keyCookie.value}`}
-        />
-      </div>
-    </main>
-  );
 }
