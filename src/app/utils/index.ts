ae5a12ccd3ab3472a50e187aca23a897e13411d8
Ryan Punwasi <ryan.punwasi@gmail.com>
2024-07-17 14:29:57 -0400

    Add encryption helper class

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,3 +1,4 @@
+import crypto from "crypto";
 export function formatBytes(bytes: number, decimals = 2) {
   if (!+bytes) return "0 Bytes";
 
@@ -18,4 +19,41 @@ export function formatBytes(bytes: number, decimals = 2) {
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
   return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+}
+
+export class AES {
+  private key: Buffer;
+  private iv: Buffer;
+
+  constructor(key: string) {
+    this.key = crypto.randomBytes(32);
+    this.iv = crypto.randomBytes(16);
+  }
+
+  get IV() {
+    return this.iv;
+  }
+
+  encrypt(text: string) {
+    let cipher = crypto.createCipheriv(
+      "aes-256-cbc",
+      Buffer.from(this.key),
+      this.iv
+    );
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return encrypted.toString("hex");
+  }
+
+  decrypt(text: string) {
+    let encryptedText = Buffer.from(text, "hex");
+    let decipher = crypto.createDecipheriv(
+      "aes-256-cbc",
+      Buffer.from(this.key),
+      this.iv
+    );
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+  }
 }
