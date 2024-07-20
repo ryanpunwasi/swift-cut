f5ebcb214c3e7eec1dda1404c424deca8e4563d3
Ryan Punwasi <ryan.punwasi@gmail.com>
2024-07-19 20:21:44 -0400

    Fix body validation errors thrown by API Gateway

--- src/app/actions/sendMessage.ts
+++ src/app/actions/sendMessage.ts
@@ -4,16 +4,25 @@ import { redirect } from "next/navigation";
 
 export async function sendMessage(formData: FormData) {
   const s3Key = formData.get("s3Key") as string;
-  const start = formData.get("start") as string;
-  const end = formData.get("end") as string;
+  const start = formData.get("start")?.toString();
+  const stop = formData.get("end")?.toString();
   let success = false;
+
+  if (start === undefined || stop === undefined || !s3Key) {
+    console.error("Missing required fields");
+    return;
+  }
 
   try {
     await fetch(process.env.NEXT_PUBLIC_AWS_API_GATEWAY_URL as string, {
       method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({
-        start,
-        end,
+        start: parseFloat(start),
+        stop: parseFloat(stop),
         s3Key,
       }),
     });
